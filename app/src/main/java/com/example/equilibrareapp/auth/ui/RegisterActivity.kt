d3a0d26ac94955408a130573c4e8cd8b72275539
Himana <himana.on@gmail.com>
2024-06-17 18:56:56 +0700

    merubah logic register

--- app/src/main/java/com/example/equilibrareapp/auth/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/equilibrareapp/auth/ui/RegisterActivity.kt
@@ -2,40 +2,74 @@ package com.example.equilibrareapp.auth.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import com.example.equilibrareapp.R
-import com.example.equilibrareapp.auth.model.RegisterViewModel
 import com.example.equilibrareapp.databinding.ActivityRegisterBinding
+import com.example.equilibrareapp.preference.PreferenceHelper
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private val registerViewModel: RegisterViewModel by viewModels()
+    private lateinit var preferenceHelper: PreferenceHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
-        binding.lifecycleOwner = this
-        binding.registerViewModel = registerViewModel
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preferenceHelper = PreferenceHelper(this)
+        setupOnClickListener()
 
-        registerViewModel.registerSuccess.observe(this, Observer { success ->
-            if (success) {
-                Toast.makeText(this, "Registrasi Berhasil", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-                finish()
+    }
+
+    private fun setupOnClickListener() {
+        binding.btnRegister.setOnClickListener {
+            val username = binding.usernameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+//            val profileUrl =binding.usernameEditText.text.toString()
+            when {
+                username.isEmpty() -> {
+                    binding.usernameEditText.error = "Username perlu diisi"
+                }
+
+                email.isEmpty() -> {
+                    binding.emailEditText.error = "Email perlu diisi"
+                }
+
+                !email.contains("@") -> {
+                    binding.emailEditText.error = "Email tidak valid"
+                }
+
+                password.isEmpty() -> {
+                    binding.passwordEditText.error = "Password perlu diisi"
+                }
+
+                else -> {
+                    showLoading(true)
+                    register()
+                }
             }
-        })
-
-        registerViewModel.navigateToLogin.observe(this, Observer {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
             finish()
-        })
+        }
+    }
+
+    private fun register() {
+        startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        with(binding) {
+            progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+            btnLogin.isEnabled = !isLoading
+            emailEditText.isEnabled = !isLoading
+            passwordEditText.isEnabled = !isLoading
+            btnRegister.isEnabled = !isLoading
+        }
     }
 }
