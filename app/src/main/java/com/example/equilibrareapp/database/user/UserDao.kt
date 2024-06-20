c80b323f433c840a507258b70633fb506240d8e0
Dull04 <abdulazizfebriansyah@gmail.com>
2024-06-20 19:13:46 +0700

    (Update) error on the profile picture

--- app/src/main/java/com/example/equilibrareapp/database/user/UserDao.kt
+++ app/src/main/java/com/example/equilibrareapp/database/user/UserDao.kt
@@ -2,19 +2,20 @@ package com.example.equilibrareapp.database.user
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.equilibrareapp.model.User
 
 @Dao
 interface UserDao {
-    @Insert
-    suspend fun insertUser(user: User)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
 
     @Query("SELECT * FROM users WHERE uid = :uid")
     suspend fun getUserByUid(uid: String): User?
 
     @Query("DELETE FROM users WHERE uid = :uid")
     suspend fun deleteUserByUid(uid: String)
+
 
     @Query("UPDATE users SET displayName= :displayName, email= :email, photoURL= :photoURL WHERE uid= :uid")
     suspend fun updateUser(uid: String, displayName: String, email: String, photoURL: String)
